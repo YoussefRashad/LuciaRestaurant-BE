59cc0dcc2c71746058aad58b19acb63da4f30600
Youssef Rashad <youssefrashad119@gmail.com>
2021-01-26 16:01:06 +0200

    update url

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,6 +1,6 @@
 
-const FE_URL = "http://localhost:3000"
-const DB_URL = "mongodb://127.0.0.1:27017/LuciaResturent"
+// const FE_URL = "http://localhost:3000"
+// const DB_URL = "mongodb://127.0.0.1:27017/LuciaResturent"
 
 
 const FE_URL = "https://lucia-restaurant.netlify.app"
