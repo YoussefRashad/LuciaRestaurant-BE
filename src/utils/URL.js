bd8d8583ff208ec75d46780648c5cd1621334770
Youssef Rashad <youssefrashad119@gmail.com>
2021-01-26 15:47:59 +0200

    using production urls

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,10 +1,10 @@
 
-const FE_URL = "http://localhost:3000"
-const DB_URL = "mongodb://127.0.0.1:27017/LuciaResturent"
+// const FE_URL = "http://localhost:3000"
+// const DB_URL = "mongodb://127.0.0.1:27017/LuciaResturent"
 
 
-// const FE_URL = "https://lucia-restaurant.netlify.app"
-// const DB_URL = ""
+const FE_URL = "https://lucia-restaurant.netlify.app"
+const DB_URL = "mongodb+srv://YoussefRashad:0000112@cluster0.p5jcz.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 module.exports = {
     FE_URL,
