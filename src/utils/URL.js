944230b3be893d9620283e4db44e6ff5715e87a4
Youssef Rashad <40126657+YoussefRashad@users.noreply.github.com>
2021-02-13 21:13:28 +0200

    Update URL.js

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -3,9 +3,9 @@
 // const DB_URL = "mongodb://127.0.0.1:27017/LuciaResturent"
 
 
-const DB_URL = "mongodb+srv://YoussefRashad:0000112@cluster0.p5jcz.mongodb.net/Lucia-Restaurant?retryWrites=true&w=majority"
+
 const FE_URL = "https://lucia-restaurant.netlify.app"
-// const DB_URL = "mongodb+srv://<userName>:<password>@cluster0.p5jcz.mongodb.net/Lucia-Restaurant?retryWrites=true&w=majority".replace("<password>", process.env.DB_PASSWORD).replace('<userName>', process.env.DB_USERNAME)
+const DB_URL = "mongodb+srv://<userName>:<password>@cluster0.p5jcz.mongodb.net/Lucia-Restaurant?retryWrites=true&w=majority".replace("<password>", process.env.DB_PASSWORD).replace('<userName>', process.env.DB_USERNAME)
 
 module.exports = {
     FE_URL,
